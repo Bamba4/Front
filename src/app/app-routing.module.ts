3ff343c8d0a1e12d2391f3a8d901cf1682a31cba
Elhadji <elhadji@ctsfares.com>
2020-07-23 10:58:11 -0400

    update map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import {HeroeDetailComponent} from './heroe-detail/heroe-detail.component';
 
 const routes: Routes = [
         { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-        {path: "heroes", component: HeroesComponent},
+        {path: 'heroes', component: HeroesComponent},
         { path: 'dashboard', component: DashboardComponent },
         { path: 'detail/:id', component: HeroeDetailComponent },
 ];
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -25,7 +25,41 @@ export class DashboardComponent implements OnInit {
     });
     this.createMarker(-75.7611979, 45.3516034);
 
+    const start = [-74.5, 40];
+    const end = [74.5, 40];
+    let isAtStart = true;
 
+    document.getElementById('fly').addEventListener('click', () => {
+      // depending on whether we're currently at point a or b, aim for
+      // point a or b
+      const target = isAtStart ? end : start;
+
+      // and now we're at the opposite point
+      isAtStart = !isAtStart;
+      const marker = this.createMarker(74.5, 40);
+      this.mapa.flyTo({
+        // These options control the ending camera position: centered at
+        // the target, at zoom level 9, and north up.
+        center: target,
+        zoom: 9,
+        bearing: 0,
+
+        // These options control the flight curve, making it move
+        // slowly and zoom out almost completely before starting
+        // to pan.
+        speed: 0.2, // make the flying slow
+        curve: 1, // change the speed at which it zooms out
+
+        // This can be any easing function: it takes a number between
+        // 0 and 1 and returns another number between 0 and 1.
+        easing: (t) => {
+          return t;
+        },
+
+        // this animation is considered essential with respect to prefers-reduced-motion
+        essential: true
+      });
+    });
   }
 
   createMarker(lng: number, lat: number) {
